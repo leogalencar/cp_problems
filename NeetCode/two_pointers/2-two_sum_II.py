d0421d727d110f5bfef9c6736054e451f225a16a
leogalencar <leonardogalencar444@gmail.com>
2026-05-25 11:30:52 -0300

    add 2-two_sum_II.py

---
+++ NeetCode/two_pointers/2-two_sum_II.py
@@ -0,0 +1,18 @@
+# Solution with two pointers approach
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            if (numbers[l] + numbers[r]) == target:
+                return [l + 1, r + 1]
+            elif (numbers[l] + numbers[r]) > target:
+                r -= 1
+            else:
+                l += 1
+
+        return []
